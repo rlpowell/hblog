21bb0728afe0a86eb2967d6bd8e906fec5934267
Robin Lee Powell <rlpowell@digitalkingdom.org>
2016-03-24 01:09:10 -0700

    Routine checkin.

---
+++ src/hblog.hs
@@ -0,0 +1,212 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleContexts #-}
+import           Data.Monoid (mappend)
+import           Data.List
+import           Text.Regex.PCRE.Heavy as PCRE
+import           Data.String.Conversions
+import           Data.Maybe (listToMaybe)
+import           Control.Monad
+import           Hakyll
+import           Hakyll.Web.Tags
+import           System.FilePath
+import           System.Posix.Files
+import           Hakyll.Web.Template.Context
+import           Text.Blaze.Html                 (toHtml, toValue, (!))
+import           Text.Blaze.Html.Renderer.String (renderHtml)
+import qualified Text.Blaze.Html5                as H
+import qualified Text.Blaze.Html5.Attributes     as A
+import qualified Data.Map                        as M
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    tags <- buildTagsWith myGetTags "posts/**" (fromCapture "tags/*.html")
+
+    categories <- buildTagsWith myGetCategory "posts/**" (fromCapture "categories/*.html")
+
+    ids <- getMatches "posts/**"
+
+    titles <- mapM getTitle ids
+
+    match "posts/**" $ do
+        route $ setExtension "html"
+        compile $ do
+            wikiCompiler titles
+            -- pandocCompiler
+            >>= renderPandoc
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtx tags categories)
+            >>= loadAndApplyTemplate "templates/default.html" (postCtx tags categories)
+            >>= relativizeUrls
+
+--     create ["categories.html"] $ do
+--         route idRoute
+--         compile $ do
+--             posts <- recentFirst =<< loadAll "categories/*"
+--             let archiveCtx =
+--                     listField "posts" (postCtx tags categories) (return posts) `mappend`
+--                     constField "title" "Archives"            `mappend`
+--                     defaultContext
+-- 
+--             makeItem ""
+--                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+--                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+--                 >>= relativizeUrls
+-- 
+-- 
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/**"
+            let archiveCtx =
+                    listField "posts" (postCtx tags categories) (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/**"
+            let indexCtx = mconcat [
+                    listField "posts" (postCtx tags categories) (return posts)
+                    , tagCloudField "tags" 120 200 tags
+                    , tagCloudField "categories" 120 200 categories
+                    , constField "title" "Home"
+                    , defaultContext
+                    ]
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+    -- Post categories
+    tagsRules categories $ \category pattern -> do
+        let title = "Posts in category " ++ category
+
+        -- Copied from posts, need to refactor
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll pattern
+            let ctx = constField "title" title `mappend`
+                        listField "posts" (postCtx tags categories) (return posts) `mappend`
+                        defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/post-list.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
+
+    -- Post tags
+    tagsRules tags $ \tag pattern -> do
+        let title = "Posts tagged " ++ tag
+
+        -- Copied from posts, need to refactor
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll pattern
+            let ctx = constField "title" title `mappend`
+                        listField "posts" (postCtx tags categories) (return posts) `mappend`
+                        defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/post-list.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
+
+        -- -- Create RSS feed as well
+        -- version "rss" $ do
+        --     route   $ setExtension "xml"
+        --     compile $ loadAllSnapshots pattern "content"
+        --         >>= fmap (take 10) . recentFirst
+        --         >>= renderRss (feedConfiguration title) feedCtx
+
+
+--------------------------------------------------------------------------------
+postCtx :: Tags -> Tags -> Context String
+postCtx tags categories = mconcat [
+    dateField "date" "%B %e, %Y"
+    , myCategoryField "category" categories
+    , tagsField "tags" tags
+    , defaultContext
+    ]        
+
+-- Pulls the title metadata out for an item
+getTitle :: MonadMetadata m => Identifier -> m String
+getTitle identifier = do
+    metadata <- getMetadata identifier
+    return $ maybe [] id $ M.lookup "title" metadata
+
+-- Get the first directory name after posts/ , call that the category
+myGetCategory :: MonadMetadata m => Identifier -> m [String]
+myGetCategory x = return $ take 1 $ drop 1 $ dropWhile (/= "posts") $ splitDirectories $ toFilePath x
+
+-- | Render the category in a link; mostly copied from https://github.com/jaspervdj/hakyll/blob/ea7d97498275a23fbda06e168904ee261f29594e/src/Hakyll/Web/Tags.hs
+myCategoryField :: String -> Tags -> Context a
+myCategoryField = tagsFieldWith myGetCategory simpleRenderLink (mconcat)
+
+-- | Render one tag link ; copied from https://github.com/jaspervdj/hakyll/blob/ea7d97498275a23fbda06e168904ee261f29594e/src/Hakyll/Web/Tags.hs
+simpleRenderLink :: String -> (Maybe FilePath) -> Maybe H.Html
+simpleRenderLink _   Nothing         = Nothing
+simpleRenderLink tag (Just filePath) =
+  Just $ H.a ! A.href (toValue $ toUrl filePath) $ toHtml tag
+
+-- | Obtain tags from a page in the default way: parse them from the @tags@
+-- metadata field.
+--
+-- FIXME: exmaple
+myGetTags :: MonadMetadata m => Identifier -> m [String]
+myGetTags identifier = do
+    metadata <- getMetadata identifier
+    let maintags = map (intercalate "+") $ drop 1 $ subsequences $ sort $ maybe [] (map trim . splitAll ",") $ M.lookup "tags" metadata
+    cat <- myGetCategory identifier
+    return $ maintags ++ (map (\x -> (head cat) ++ ":" ++ x) maintags)
+
+-- wikiCompiler :: [String] -> Item String -> Compiler (Item String)
+-- wikiCompiler titles (Item i x) = do
+--     return (Item i ((toFilePath i) ++ (intercalate " -- " titles)))
+
+wikiCompiler :: [String] -> Compiler (Item String)
+wikiCompiler titles = do
+    -- FIXME: describe
+    filePath <- toFilePath <$> getUnderlying
+    -- FIXME: describe
+    makeItem =<< unsafeCompiler (unWiki titles filePath)
+
+unWiki :: [String] -> String -> IO String
+unWiki titles filePath = do
+    body <- readFile filePath
+    case filePath of
+    -- FIXME: remove
+      "posts/personal/my_views_on_the_future.md" ->
+          let newBody = gsub [re|\s(qq[a-z]+)\s(.*?)\sqq\s|] (unWikiReplacer titles) body in do
+            _ <- writeFile (filePath ++ ".hblog.tmp") newBody
+            rename (filePath ++ ".hblog.tmp") filePath
+            return newBody
+      _ -> return body
+
+unWikiReplacer :: [String] -> String -> [String] -> String
+unWikiReplacer titles match subList = case length subList > 0 of
+        True -> " --" ++ subList!!1 ++ "-- "
+        False -> match
---
+++ src/tiki_to_md.hs
@@ -0,0 +1,76 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleContexts #-}
+import           Data.Monoid (mappend)
+import           Data.List
+import           Text.Regex.PCRE.Heavy as PCRE
+import           Data.Maybe (listToMaybe)
+import           System.FilePath
+import           System.Posix.Files
+import           System.Environment
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = do
+  args <- getArgs
+  let filePath = head args in do
+    body <- readFile filePath
+    let newBody = unTiki body in do
+      _ <- writeFile (filePath ++ ".hblog.tmp") newBody
+      -- rename (filePath ++ ".hblog.tmp") filePath
+      return ()
+
+unTiki :: String -> String
+unTiki oldBody = if newBody == oldBody then
+    newBody
+  else
+    unTiki newBody
+  where
+    -- FIXME: For bonus points: bail on dead links!
+    newBody = 
+      -- Bold   __text__
+      -- Centered       ::text::
+      -- Italic ''text''
+      -- Monospaced     -+text+-
+      -- Underlined     ===text===
+      -- Text box       ^text^
+      -- Deleted        --text--
+      -- Display syntax ~np~__not bold__~/np~
+      -- superscript: {TAG(tag=>sup)}super{TAG} / {SUP()}...{SUP}
+      -- subscript: {TAG(tag=>sub)}sub{TAG} / {SUB()}...{SUB}
+      -- headings: ! , !! , ...
+      -- line break: %%%
+      -- square brackets: [[not a link]
+      -- non-breaking space: ~hs~
+      -- html comments: ~hc~...~/hc~
+      -- code: {CODE()}...{CODE}
+      -- internal link: ((PluginVersions|The Versions Plugin)) ; ((Using Wiki Pages|#The_Wiki_Menu|The Wiki Menu)) for anchors
+      -- Link to a heading on the same page: [#Related_Pages|Related Pages]
+      -- Link to a Web page: [http://www.somesite.org] or [http://www.somesite.org|Some Site]
+      -- Table of contents: {maketoc}
+      -- Bulleted list: *item
+      -- Numbered list: #item
+      -- Definition list: ;term:definition
+      -- Horizontal line: ----
+
+      -- Special characters: ~126~
+      -- For this one, maybe just bail and alert the user.
+
+      -- Tables: ||row1-column1|row1-column2||row2-column1|row2-column2||
+      -- For this one, maybe just bail and alert the user.
+
+      -- Image display: {img attId="39" imalign="right" link="http://info.tikiwiki.org" alt="Panama Hat"} / {img attId="37", thumb="mouseover", styleimage="border", desc="desc", max="150"} / {img src="img/wiki_up/393px-Pears.jpg" thumb="y" imalign="center" stylebox="border" button="y" desc="Pretty pears" max="200" rel="box"}
+      -- For this one, maybe just bail and alert the user.
+
+      -- ~white,white:text~~ ; colored text
+      gsub [re|~~([^,~:]*):(.*)~~|] (\(color:text:_) -> "<div style='color: " ++ color ++ "'>" ++ text ++ "</div>" :: String ) $
+      gsub [re|~~([^,~:]*),([^~]*):(.*)~~|] (\(color:bgcolor:text:_) -> "<div style='color: " ++ color ++ "; background-color: " ++ bgcolor ++ "'>" ++ text ++ "</div>" :: String ) $
+      -- ~tc~...~/tc~ ; tiki comments ; we turn these into YAML metadata
+      -- comments per https://groups.google.com/forum/#!topic/pandoc-discuss/xL05J-_4YHs ; they look like:
+      -- ---
+      -- # foo
+      -- ---
+      --
+      gsub [re|~tc~((\s|\S)*?)~/tc~|] (\(text:_) -> "\n\n---\n" ++ (unlines $ map (\x -> "# " ++ x) $ lines text) ++ "\n---\n\n" :: String) $
+      oldBody
