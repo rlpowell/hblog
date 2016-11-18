9994a8a326794c981b03af8025319df35eb4dc8f
Robin Lee Powell <rlpowell@digitalkingdom.org>
2016-11-17 17:26:01 -0800

    Doc update.

--- lib/HBlog/Lib.hs
+++ lib/HBlog/Lib.hs
@@ -9,5 +9,6 @@ import          Text.Regex.PCRE.Heavy as PCRE
 import          Text.Regex.PCRE.Light.Base as PCREL
 import          Language.Haskell.TH.Quote
 
+-- Case-insensitive regular expression quasi-quoter, used in unphone
 caselessre :: QuasiQuoter
 caselessre = PCRE.mkRegexQQ [utf8, caseless]
