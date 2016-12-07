d39ab2de98d3f91186ef735e9db493c2de5c4ccd
Robin Lee Powell <rlpowell@digitalkingdom.org>
2016-12-06 17:42:19 -0800

    The site builder script.

---
+++ run_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ ! -f run_docker.sh ]
+then
+  echo "You seem to be in the wrong place."
+  exit 1
+fi
+
+rm -f posts
+if [ -e posts ]
+then
+  echo "Couldn't remove the posts link"
+  exit 1
+fi
+
+ln -s /dropbox/Docs/Public/hblog_posts posts
+
+rm -rf _site
+rm -rf _cache
+
+hblog build
+
+rm -rf _cache
