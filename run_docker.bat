46b0db85e8238cca8511d1adfe35023169d13582
Robin Lee Powell <rlpowell@digitalkingdom.org>
2017-02-27 18:41:45 -0800

    I'm always tweaking these guys, but might as well check them in.

--- run_docker.bat
+++ run_docker.bat
@@ -1,7 +1,7 @@
 docker kill hblog
 docker rm hblog
 docker build -t rlpowell/hblog .
-docker run --name hblog -v /var/run/docker.sock:/var/run/docker.sock -v C:\Users\rlpowell\Dropbox:/dropbox:z --volumes-from stackstore --volumes-from stackstore-hblog-1 -p 0.0.0.0:2224:22 -p 0.0.0.0:8084:8084 -d -t -i rlpowell/hblog bash -x /tmp/docker_run_init.sh
+docker run --name hblog -v /var/run/docker.sock:/var/run/docker.sock -v C:\Users\rlpowell\Dropbox:/dropbox:z --volumes-from volume--stack--global --volumes-from volume--local--hblog --volumes-from volume--stack-work--hblog -p 0.0.0.0:2224:22 -p 0.0.0.0:8084:8084 -t -i -d rlpowell/hblog bash -x /tmp/docker_run_init.sh
 timeout /nobreak /t 5
 docker logs hblog
 docker ps -a -f name=hblog
--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 sudo docker kill hblog
 sudo docker rm hblog
 sudo docker build -t rlpowell/hblog .
-sudo docker run --name hblog -v /var/run/docker.sock:/var/run/docker.sock -v /home/rlpowell/Dropbox:/dropbox:z --volumes-from stackstore --volumes-from stackstore-hblog-1 -p 0.0.0.0:2224:22 -p 0.0.0.0:8084:8084 -d -t -i rlpowell/hblog bash -x /tmp/docker_run_init.sh
+sudo docker run --name hblog -v /var/run/docker.sock:/var/run/docker.sock -v /home/rlpowell/Dropbox:/dropbox:z --volumes-from volume--stack--global --volumes-from volume--local--hblog --volumes-from volume--stack-work--hblog -p 0.0.0.0:2224:22 -p 0.0.0.0:8084:8084 -d -t -i rlpowell/hblog bash -x /tmp/docker_run_init.sh
 sleep 5
 docker logs hblog
 docker ps -a -f name=hblog
