cb9816620ffb951b33ffee4af290ccc7ef069541
Robin Lee Powell <rlpowell@digitalkingdom.org>
2017-03-04 17:52:04 -0800

    Support for direct file name usage.  Stopped producing the HTML versions as that was mostly for testing during active development.

--- app/tiki_to_md.hs
+++ app/tiki_to_md.hs
@@ -7,8 +7,8 @@ import System.FilePath (makeRelative, (</>), takeDirectory)
 import System.Environment (getArgs)
 import qualified System.FilePath.Find as F (find, always, extension)
 import System.FilePath.Find ((==?))
-import System.Directory (createDirectoryIfMissing)
-import Text.Pandoc (readerStandalone, def, writeMarkdown, writeHtmlString, writerStandalone, writerTemplate, Pandoc(..))
+import System.Directory (createDirectoryIfMissing, doesFileExist)
+import Text.Pandoc (readerStandalone, def, writeMarkdown, writerStandalone, writerTemplate, Pandoc(..))
 import Text.Pandoc.Templates (getDefaultTemplate)
 import Text.Pandoc.Readers.TikiWiki (readTikiWiki)
 import Data.List (intercalate)
@@ -19,37 +19,48 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    indir:outdir:[] -> walkTree indir outdir
+    indir:outdir:[] -> maybeTree indir outdir
     _               -> putStrLn "Need exactly two arguments, input directory and output directory."
+
+maybeTree :: String -> String -> IO ()
+maybeTree inthing outthing = do
+  isInFile <- doesFileExist inthing
+  if isInFile then
+    handleFile inthing outthing
+  else
+    walkTree inthing outthing
 
 walkTree :: String -> String -> IO ()
 walkTree indir outdir = do
   putStrLn $ "Searching for files ending in .tiki"
   files <- F.find F.always (F.extension ==? ".tiki") indir 
   putStrLn $ "Files found: " ++ (intercalate " " files)
-  _ <- mapM (handleFile indir outdir) files
+  _ <- mapM (handleFileAndDir indir outdir) files
   return ()
 
-handleFile :: FilePath -> FilePath -> FilePath -> IO ()
-handleFile indir outdir file = do
-  putStrLn $ "Processing file " ++ file
-  let shortname = makeRelative indir file
-  let shortnameBase = maybe (error $ "Can't strip .tiki from file " ++ file) T.unpack $
-        T.stripSuffix ".tiki" $ T.pack $ makeRelative indir file
-  body <- readFile $ indir </> shortname
+handleFileAndDir :: FilePath -> FilePath -> FilePath -> IO ()
+handleFileAndDir indir outdir fpath = do
+  let shortname = makeRelative indir fpath
+  let shortnameBase = maybe (error $ "Can't strip .tiki from file name " ++ fpath) T.unpack $
+        T.stripSuffix ".tiki" $ T.pack $ shortname
+  _ <- createDirectoryIfMissing True (takeDirectory $ (outdir </> shortname))
+  handleFile (indir </> shortname) (outdir </> shortnameBase ++ ".md")
+
+handleFile :: FilePath -> FilePath -> IO ()
+handleFile infile outfileMD = do
+  putStrLn $ "Processing file " ++ infile
+  body <- readFile $ infile
   if T.isInfixOf "~tc~" (T.pack body) then
-    error $ "We do not support ~tc~ style comments, in file " ++ file ++ " , for the reasons I (rlpowell) described at https://github.com/jgm/pandoc/issues/2552 .  Erroring out to stop you from accidentally revealing something secret."
+    error $ "We do not support ~tc~ style comments, in file " ++ infile ++ " , for the reasons I (rlpowell) described at https://github.com/jgm/pandoc/issues/2552 .  Erroring out to stop you from accidentally revealing something secret."
   else
     do
       Right mdTemplate <- getDefaultTemplate Nothing "markdown"
-      let pandoc = unTiki file mdTemplate body in do
-        _ <- createDirectoryIfMissing True (takeDirectory $ (outdir </> shortname))
-        _ <- writeFile (outdir </> shortnameBase ++ ".md") $ writeMarkdown def { writerStandalone = True , writerTemplate = mdTemplate } pandoc
-        _ <- writeFile (outdir </> shortnameBase ++ ".html") $ writeHtmlString def { writerStandalone = True , writerTemplate = mdTemplate } pandoc
+      let pandoc = unTiki infile body in do
+        _ <- writeFile outfileMD $ writeMarkdown def { writerStandalone = True , writerTemplate = mdTemplate } pandoc
         return ()
 
-unTiki :: FilePath -> String -> String -> Pandoc
-unTiki filePath mdTemplate body =
+unTiki :: FilePath -> String -> Pandoc
+unTiki filePath body =
   let pandocEither = readTikiWiki def { readerStandalone = True } body in
     case pandocEither of
       Left e -> error $ "Pandoc error! on file " ++ filePath ++ ": " ++ (show e)
