328dd8f086ee956283f690d0cf4f905c93ff22a0
Robin Lee Powell <rlpowell@digitalkingdom.org>
2017-07-24 08:46:54 -0700

    Make tests run a bit more reliably on Windows+Docker.

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,14 @@
 # Make sure our non-stack script is in a place stack can find it
 ln -sf /home/rlpowell/src/hblog/munge_files.sh ~/.local/bin/munge_files
 
+find /home/rlpowell/src/hblog/tests/ /dropbox/src/hblog/tests/   -type f | xargs chmod a-x
+
 echo "Unpacking git zips."
 oldpwd=$(pwd)
 find tests/ -name git.zip | while read zipname
 do
   cd "$(dirname $zipname)"
+  find . -name .git | xargs rm -rf
   unzip "$(basename $zipname)" >/dev/null
   cd "$oldpwd"
 done
