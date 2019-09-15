b6a2191fa715295f8d328389985dec0e293a38db
Robin Lee Powell <rlpowell@digitalkingdom.org>
2019-09-14 23:21:42 -0700

    Slightly cleaner output.

--- hbgwdiff.sh
+++ hbgwdiff.sh
@@ -5,6 +5,8 @@
 # function.
 
 echo "You can pipe the wdiff output below through colordiff --difftype=wdiff for color."
-echo "BEGIN wdiffs for $1"
-wdiff -n $2 $5 | (grep --color=never -C 5 '[{[][+-]' || echo "No wdiffs found.")
-echo "END wdiffs for $1"
+echo
+echo "----------- BEGIN wdiffs for $1"
+wdiff -n $2 $5 | grep --color=never -C 5 '[{[][+-]'
+echo "----------- END wdiffs for $1"
+echo
