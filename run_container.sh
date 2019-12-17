613406a5961864dddfe1871c3d044cee196dfd90
Robin Lee Powell <rlpowell@digitalkingdom.org>
2019-12-16 17:41:59 -0800

    Dropped the rootless podman sudo hack; it's been moved into a systemscript.

--- run_container.sh
+++ run_container.sh
@@ -12,10 +12,10 @@ CONTAINER_BIN=${CONTAINER_BIN:-$(which docker)}
 
 "$(dirname $0)"/stop_container.sh || true
 
-sudo -u rlpowell $CONTAINER_BIN run --userns=keep-id --name hblog -v ~/public_html/hblog:/web:rw -v /home/rlpowell:/home/rlpowell:rw -p 2224:22 -p 8084:8084 -d -t -i rlpowell/hblog bash -x container_run_init.sh
+$CONTAINER_BIN run --userns=keep-id --name hblog -v ~/public_html/hblog:/web:rw -v /home/rlpowell:/home/rlpowell:rw -p 2224:22 -p 8084:8084 -d -t -i rlpowell/hblog bash -x container_run_init.sh
 sleep 5
-sudo -u rlpowell $CONTAINER_BIN logs hblog
-sudo -u rlpowell $CONTAINER_BIN ps -a -f name=hblog
+$CONTAINER_BIN logs hblog
+$CONTAINER_BIN ps -a -f name=hblog
 echo
 echo
 echo Now connect with SSH
